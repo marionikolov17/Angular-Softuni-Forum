25b190437b3407011218de19fb5bc5b347699e89
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-01 13:18:03 +0200

    Create and implement ApiService

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { MainComponent } from './main/main.component';
 import { ThemesListComponent } from './main/themes-list/themes-list.component';
 import { PostsListComponent } from './main/posts-list/posts-list.component';
+import { SharedModule } from './shared/shared.module';
+import { ApiService } from './api.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,11 @@ import { PostsListComponent } from './main/posts-list/posts-list.component';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    SharedModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
