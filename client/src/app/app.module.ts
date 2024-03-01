80dbcfbcbf03c8463dee32165ad19af1184f6175
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-01 12:47:03 +0200

    Update styles

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
