eb3afe0799ad4171202e2c5dceff365431d4f039
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-01 13:27:48 +0200

    Update api.service.ts

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -2,9 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  api: string = 'http://localhost:3000/api';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  public getThemes() {
+    return this.http.get(this.api + "/themes");
+  }
+
+  public getPosts(limit: number | undefined) {
+    let url: string = this.api + "/posts";
+
+    if (limit)
+      url += `?limit=${limit}`;
+
+    return this.http.get(url);
+  }
 }
