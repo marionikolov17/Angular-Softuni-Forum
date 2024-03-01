a98bd6f91c314b8ff95e71fdb9f77a08009c02ce
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-01 13:28:01 +0200

    Update themes-list.component.ts

--- client/src/app/main/themes-list/themes-list.component.ts
+++ client/src/app/main/themes-list/themes-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-themes-list',
   templateUrl: './themes-list.component.html',
   styleUrls: ['./themes-list.component.css']
 })
-export class ThemesListComponent {
+export class ThemesListComponent implements OnInit {
+  themes: any[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+      this.apiService.getThemes().subscribe((themes) => {
+        console.log(themes);
+      })
+  }
 }
