95efdb6ceb838b60ce6653944c268ebbbbfb6fe5
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-12 12:37:14 +0200

    Create Profile component

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
