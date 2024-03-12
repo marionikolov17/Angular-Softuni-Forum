91e8547365b541decb46de3dfc9427ae41cc67ac
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-12 12:47:41 +0200

    Create Welcome-Message Component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
+import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
 
 
 
 @NgModule({
   declarations: [
-    LoaderComponent
+    LoaderComponent,
+    WelcomeMsgComponent
   ],
   imports: [
     CommonModule
