9560c8626ffb92e5a03fde5bcf65da2c7f1bfea8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-12 11:47:28 +0200

    Implement routing logic

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
-
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/home" },
+  { path: "home", component: MainComponent }
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
