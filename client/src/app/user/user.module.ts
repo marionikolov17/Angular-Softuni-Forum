7e8b4a8d959308d4edd411dd31a9cd980ebcc1c1
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-12 12:36:09 +0200

    Create Login component

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
