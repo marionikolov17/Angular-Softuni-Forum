7caa36f563d04ec5a5003c57677e2fc539afffec
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-13 13:39:45 +0200

    Make Current Theme path

--- client/src/app/theme/theme-routing.module.ts
+++ client/src/app/theme/theme-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { NewThemeComponent } from './new-theme/new-theme.component';
+import { CurrentThemeComponent } from './current-theme/current-theme.component';
 
 const routes: Routes = [
   {
     path: 'themes',
     children: [
       { path: "", pathMatch: "full", component: MainComponent },
-      { path: "add-theme", component: NewThemeComponent }
+      { path: "add-theme", component: NewThemeComponent },
+      { path: ":themeId", component: CurrentThemeComponent }
     ]
   },
 ];
