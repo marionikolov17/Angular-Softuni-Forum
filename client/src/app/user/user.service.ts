3745fa631a3a803df59f2c08fb0f07feb036d6e2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-13 12:20:17 +0200

    Update user.service.ts

--- client/src/app/user/user.service.ts
+++ client/src/app/user/user.service.ts
@@ -3,7 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserServiceService {
 
+  public isLoggedIn: boolean = false;
+
   constructor() { }
+
+  public login(): void {
+    this.isLoggedIn = true;
+  }
+
+  public logout(): void {
+    this.isLoggedIn = false;
+  }
+
 }
