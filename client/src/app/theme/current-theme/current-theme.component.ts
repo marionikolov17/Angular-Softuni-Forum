2c906d8feddf803ad0895f1d3d7765a52b31453f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-13 18:15:22 +0200

    Update current-theme.component.ts

--- client/src/app/theme/current-theme/current-theme.component.ts
+++ client/src/app/theme/current-theme/current-theme.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { Theme } from 'src/app/types/theme';
 
 @Component({
   selector: 'app-current-theme',
   templateUrl: './current-theme.component.html',
   styleUrls: ['./current-theme.component.css']
 })
-export class CurrentThemeComponent {
+export class CurrentThemeComponent implements OnInit {
+  theme = {} as Theme;
+  constructor(private apiService: ApiService, private activeRoute: ActivatedRoute) {}
 
+  ngOnInit(): void {
+      this.activeRoute.params.subscribe(data => {
+        const themeId = data["themeId"];
+
+        this.apiService.getTheme(themeId).subscribe(theme => {
+          console.log(theme);
+          this.theme = theme;
+        })
+      })
+  }
 }
