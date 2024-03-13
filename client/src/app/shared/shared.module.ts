bfe9556b844a79ab10458a7a0b33e04281a8a846
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-13 12:48:25 +0200

    Update shared.module.ts

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,8 @@ import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
     WelcomeMsgComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     LoaderComponent,
