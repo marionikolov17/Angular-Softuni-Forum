2423831e195971d67fba70d056c8334bb1ac0556
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-17 14:57:46 +0200

    Update email-validator.ts

--- client/src/app/shared/validators/email-validator.ts
+++ client/src/app/shared/validators/email-validator.ts
@@ -3,6 +3,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 export function emailValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const regEx = /.{6,}@gmail.(com|bg)/gm;
+    if (control.value === "") return null;
     return regEx.test(control.value) ? null : { emailValidator: true }
   }
 }
