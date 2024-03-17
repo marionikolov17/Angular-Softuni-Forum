c63d7e92af4dcbd43e03dc63085442c20be66b86
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-17 15:31:16 +0200

    Create passwords-match-validator.ts

---
+++ client/src/app/shared/validators/passwords-match-validator.ts
@@ -0,0 +1,11 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+
+export function matchPasswordsValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const passwordControl = control.get("password");
+    const rePasswordControl = control.get("rePassword");
+
+    return passwordControl?.value === rePasswordControl?.value ? null : { matchPasswordsValidator: true };
+  }
+}
