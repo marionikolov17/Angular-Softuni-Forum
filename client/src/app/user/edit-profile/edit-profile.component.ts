8bdd44d14bee4a7a0a4d4fda5b935090fa035fd2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-17 17:12:02 +0200

    Import user service in edit profile

--- client/src/app/user/edit-profile/edit-profile.component.ts
+++ client/src/app/user/edit-profile/edit-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent {
-
+  constructor(public userService: UserService) {}
 }
