eb77864209613c55dd457241262b127de9cb63d7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-17 17:58:26 +0200

    Update email.directive.ts

--- client/src/app/shared/validators/email.directive.ts
+++ client/src/app/shared/validators/email.directive.ts
@@ -1,12 +1,20 @@
 import { Directive } from '@angular/core';
-import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appEmail]'
+  selector: '[appEmail]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: EmailDirective,
+      multi: true
+    }
+  ]
 })
 export class EmailDirective implements Validator {
 
   validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    console.log(control)
     const regEx = /.{6,}@gmail.(com|bg)/gm;
     if (control.value === "") return null;
     return regEx.test(control.value) ? null : { emailValidator: true }
