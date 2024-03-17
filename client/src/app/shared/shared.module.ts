653276ea62e1b3fa192774cb9cc601246c6b6978
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-17 17:51:17 +0200

    Create email directive for validation

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
 import { RouterModule } from '@angular/router';
+import { EmailDirective } from './validators/email.directive';
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
     WelcomeMsgComponent,
+    EmailDirective,
   ],
   imports: [
     CommonModule,
