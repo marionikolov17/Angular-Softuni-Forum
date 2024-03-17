d5e5b77450498728afbf7509d9b4f6b7391ba7b4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-17 16:47:47 +0200

    Implement Route Guard in user routes

--- client/src/app/user/user-routing.module.ts
+++ client/src/app/user/user-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthActivate } from '../guards/auth.activate';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  { path: "profile", component: ProfileComponent }
+  { path: "profile", component: ProfileComponent, canActivate: [AuthActivate] }
 ];
 
 @NgModule({
