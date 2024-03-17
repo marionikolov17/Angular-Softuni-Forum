4f4188f2766ac702dc51078c09d32afc9d1723c3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-17 15:31:40 +0200

    Create Register Reactive Form and Validation

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { emailValidator } from 'src/app/shared/validators/email-validator';
+import { matchPasswordsValidator } from 'src/app/shared/validators/passwords-match-validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  registerForm = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, emailValidator()]],
+    tel: [''],
+    passwords: this.fb.group(
+      {
+        password: ['', [Validators.required, Validators.minLength(5)]],
+        rePassword: ['', [Validators.required]],
+      },
+      {
+        validators: [matchPasswordsValidator()],
+      }
+    ),
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  get passwords() {
+    return this.registerForm.get('passwords');
+  }
+
+  register() {
+    if (this.registerForm.invalid) {
+      console.log('Register Form is invalid');
+    } else {
+      console.log('Register Form is valid');
+    }
+  }
 }
